6b6031fc4003427272cf408dfe8dd3cd2386f5b3
Nils Breunese <N.Breunese@vpro.nl>
2011-02-01 17:01:41 +0100

    Deleted readme info from source.

--- couchdb_maintenance.py
+++ couchdb_maintenance.py
@@ -19,38 +19,6 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
-#
-# INFORMATION
-# -----------
-#
-# This script performs maintenance on CouchDB servers by:
-#
-# * Compacting all databases
-# * Compacting all views for all databases
-# * Cleaning up all views
-#
-# See http://wiki.apache.org/couchdb/Compaction for more info.
-#
-# DEPENDENCIES
-# ------------
-#
-# This script depends on couchdbkit. To install or upgrade to the latest
-# version, run this:
-# 
-# $ sudo apt-get install python-setuptools
-# $ sudo easy_install -U couchdbkit
-#
-# This script also requires a supported JSON module for Python. You can use
-# cjson (or simplejson, or probably a few others if you like):
-#
-# $ sudo apt-get install python-cjson
-#
-# USAGE
-# -----
-#
-# Provide URL's to CouchDB instances as arguments to the script:
-#
-# $ ./couchdb_maintenance.py http://localhost:5984 http://example.com
 
 from couchdbkit import Server
 import logging
